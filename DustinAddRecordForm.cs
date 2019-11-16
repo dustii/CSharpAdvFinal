6128f04b8175a924f9859cf4dc89c22b48e40bf1
dustii <webber.dustin@gmail.com>
2019-11-16 17:31:27 -0600

    DustinAddRecord form is now completed.

---
+++ DustinAddRecordForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IBM.Data.DB2.iSeries;
+
+namespace CSharpAdvFinal
+{
+    public partial class DustinAddRecordForm : Form
+    {
+        public DustinAddRecordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DWAddARecord_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'deathstarDataSet.TASKS' table. You can move, or remove it, as needed.
+            this.tASKSTableAdapter.Fill(this.deathstarDataSet.TASKS);
+            // TODO: This line of code loads data into the 'deathstarDataSet.AIRPLANE' table. You can move, or remove it, as needed.
+            this.aIRPLANETableAdapter.Fill(this.deathstarDataSet.AIRPLANE);
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow newRow = deathstarDataSet.WORKORDER.NewRow();
+                newRow["orderID"] = txtWrkOrdID.Text;
+                newRow["planeNo"] = cbxPlaneNo.Text;
+                newRow["taskID"] = cbxTaskID.Text;
+                newRow["strDat"] = dteStart.Text;
+                newRow["endDat"] = dteEnd.Text;
+                newRow["woStat"] = txtStatus.Text;
+                newRow["partsLbr"] = numCost.Text;
+                newRow["lbrHours"] = numLabor.Text;
+
+                deathstarDataSet.WORKORDER.AddWORKORDERRow((DeathstarDataSet.WORKORDERRow) newRow);
+                deathstarDataSet.WORKORDER.AcceptChanges();
+                iDB2DataAdapter dataAdapter = new iDB2DataAdapter();
+                dataAdapter.Update(deathstarDataSet.WORKORDER);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace CSharpAdvFinal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new form1());
         }
     }
 }
